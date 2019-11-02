1684163ec0e588ed4c61f0a41fb756cb2648757b
Adam <beginner1986@gmail.com>
2019-11-02 09:38:58 +0100

    Add project files.

---
+++ MatrixGenerator/main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <fstream>
+
+// range of values generated in the matrix
+constexpr auto MIN_VALUE = -5.0;
+constexpr auto MAX_VALUE = 5.0;
+
+// functions declarations
+double** allocateMatrix(size_t const m, size_t const n);
+void freeMemory(double** matrix, size_t const m, size_t const n);
+void generateValues(double** matrix, size_t const m, size_t const n);
+void saveToFile(double** const matrix, size_t const m, size_t const n);
+
+// ************** MAIN **************
+int main()
+{
+	// polish fonts support
+	setlocale(LC_ALL, "polish");
+
+	bool repeat = false;
+
+	do
+	{
+		// setting matrix size (m x n)
+		// m - rows; n - columns
+		int m, n;
+		std::cout << "Podaj wymiary macierzy M x N: ";
+		std::cin >> m >> n;
+
+		// memory allocation for the matrix
+		double** matrix = allocateMatrix(m, n);
+
+		// fill the matrix with random values
+		generateValues(matrix, m, n);
+
+		// save matrix to the file
+		saveToFile(matrix, m, n);
+
+		// freeing memory after the matrix
+		freeMemory(matrix, m, n);
+
+		// ask if repetition needed
+		char answer = 'n';
+		std::cout << "Czy wygenerowaæ kolejn¹ macierz [t/n]? ";
+		std::cin >> answer;
+		repeat = (answer == 't') ? true : false;
+		std::cout << std::endl;
+	} while (repeat != false);
+
+		return 0;
+	}
+
+	// functions definitions
+	double** allocateMatrix(size_t const m, size_t const n)
+	{
+		// allocate the memory for M x N matrix
+		double** matrix = new double*[m];
+		for (size_t i = 0; i < m; i++)
+		{
+			matrix[i] = new double[n];
+		}
+
+		return matrix;
+	}
+
+	void freeMemory(double** matrix, size_t const m, size_t const n)
+	{
+		// free all the memory allocated for the matrix
+		for (size_t i = 0; i < m; i++)
+		{
+			delete[] matrix[i];
+		}
+		delete[] matrix;
+	}
+
+	void generateValues(double** matrix, size_t const m, size_t const n)
+	{
+		// RNG setup
+		std::default_random_engine generator;
+		std::uniform_real_distribution<double> distribution(MIN_VALUE, MAX_VALUE);
+
+		// generate random matrix content
+		for (size_t i = 0; i < m; i++)
+		{
+			for (size_t j = 0; j < n; j++)
+			{
+				matrix[i][j] = distribution(generator);
+			}
+		}
+	}
+
+	void saveToFile(double** const matrix, size_t const m, size_t const n)
+	{
+		// get filename
+		std::string fileName;
+		std::cout << "Podaj nazwê pliku wynikowego: ";
+		std::cin >> fileName;
+
+		// open file
+		std::fstream file;
+		file.open(fileName, std::fstream::out);
+
+		// save matrix to the file
+		// matrix size
+		file << m << " " << n << std::endl;
+
+		// matrix content
+		for (size_t i = 0; i < m; i++)
+		{
+			for (size_t j = 0; j < n; j++)
+			{
+				file << matrix[i][j] << " ";
+			}
+			file << std::endl;
+		}
+
+		// close the file
+		file.close();
+}
